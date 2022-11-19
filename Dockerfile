6705db605d17f1fd18ad9c7cc116707162d7d103
raja <1647193241@qq.com>
2022-11-19 21:51:18 +0800

    fix

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM alpine
 
 COPY entrypoint.sh /entrypoint.sh
 COPY cmd /cmd
+RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,12 @@ set -eu
 chmod +x cmd/algolia
 chmod +x cmd/jq
 
-./cmd/jq -c '.[]' $1 | ./cmd/algolia objects import $2 -F - --application-id $3 --admin-api-key $4 --auto-generate-object-id-if-not-exist
+FILE_PATH=$1
+INDEX_NAME=$2
+APPLICATION_ID=$3
+ADMIN_API_KEY=$4
+
+./cmd/jq -c '.[]' $GITHUB_WORKSPACE/$FILE_PATH | ./cmd/algolia objects import $INDEX_NAME -F - --application-id $APPLICATION_ID --admin-api-key $ADMIN_API_KEY --auto-generate-object-id-if-not-exist
 
 if [ "$?" != "0" ] ; then
   echo "😢 Failed to upload your data to Algolia, PLZ report an issue, thx!"
